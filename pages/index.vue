dd15e0c3fa975b2671e42b98a662f8315e577bd6
tss-design <h31a07@ed.cc.suzuka-ct.ac.jp>
2019-11-29 14:44:14 +0900

    :sparkles: Add reception sample

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,21 @@
 <template>
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
-      <v-card v-if="isLogin">
-        <v-card-title class="headline">
-          This is Logged in page.
-        </v-card-title>
-      </v-card>
+      <div v-if="isLogin" class="container">
+        <div>
+          <h1 class="title">
+            整理番号 1
+          </h1>
+          <h2 class="subtitle">
+            time saving system
+          </h2>
+          <div class="links">
+            <a href="https://nuxtjs.org/" class="button--red">
+              受付
+            </a>
+          </div>
+        </div>
+      </div>
       <v-card v-else>
         <v-card-title class="headline">
           This is LP.
@@ -22,3 +32,29 @@ export default {
   mixins: [apiJobMixin]
 }
 </script>
+
+<style>
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  position: relative;
+  font-weight: 300;
+  font-size: 100px;
+  color: #0d0d0e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+  font-size: 100px;
+}
+</style>
