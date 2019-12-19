a203cb8bbd34801bb857cf799f1e11514cf750cb
konnyaku256 <29503528+konnyaku256@users.noreply.github.com>
2019-12-19 16:53:49 +0900

    :sparkles: Add reference number update process

--- pages/mypage.vue
+++ pages/mypage.vue
@@ -6,15 +6,27 @@
           <v-list-item-content>
             <v-list-item-title>{{ user.name }}</v-list-item-title>
             <v-list-item-title>{{ user.email }}</v-list-item-title>
+            <v-list-item-title>{{
+              organization.organizationName
+            }}</v-list-item-title>
+            <v-list-item-title>{{
+              organization.currentTicketNumber
+            }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
+        <v-form @submit.prevent="incrementCurrentTicketNumber">
+          <v-btn color="primary" type="submit">button</v-btn>
+        </v-form>
       </v-card>
     </v-col>
   </v-row>
 </template>
 
 <script>
+import apiJobMixin from '@/mixins/apiJobMixin'
+
 export default {
+  mixins: [apiJobMixin],
   computed: {
     user() {
       if (this.$store.getters.user) {
@@ -24,6 +36,24 @@ export default {
         name: '',
         email: ''
       }
+    },
+    organization() {
+      if (this.$store.getters['organization/organization']) {
+        return this.$store.getters['organization/organization']
+      }
+      return {
+        organizationName: '',
+        currentTicketNumber: ''
+      }
+    }
+  },
+  methods: {
+    incrementCurrentTicketNumber() {
+      const payload = this.$store.getters.user
+      this.$store.dispatch('organization/incrementCurrentTicketNumber', payload)
+    },
+    jobsDone() {
+      this.removeErrors()
     }
   }
 }
