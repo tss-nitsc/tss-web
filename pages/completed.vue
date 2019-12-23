93529d45ddb33a325080fea385d2c4fc6d02d78b
konnyaku256 <29503528+konnyaku256@users.noreply.github.com>
2019-12-23 10:35:42 +0900

    :sparkles: Get store data from getters

--- pages/completed.vue
+++ pages/completed.vue
@@ -3,6 +3,16 @@
     <v-flex xs12 sm8 md6>
       <v-card v-if="isLogin">
         <v-card-title class="headline">発券完了</v-card-title>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>{{
+              organization.organizationName
+            }}</v-list-item-title>
+            <v-list-item-title>{{
+              organization.currentTicketNumber
+            }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
       </v-card>
     </v-flex>
   </v-layout>
@@ -11,6 +21,17 @@
 <script>
 import apiJobMixin from '@/mixins/apiJobMixin'
 export default {
-  mixins: [apiJobMixin]
+  mixins: [apiJobMixin],
+  computed: {
+    organization() {
+      if (this.$store.getters['organization/organization']) {
+        return this.$store.getters['organization/organization']
+      }
+      return {
+        organizationName: '',
+        currentTicketNumber: ''
+      }
+    }
+  }
 }
 </script>
