e59193383e6cb15066b4d6d1bccb8f64e016e976
konnyaku256 <29503528+konnyaku256@users.noreply.github.com>
2019-12-23 10:18:28 +0900

    :tada: Add issuing ticket completed page

---
+++ pages/completed.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <v-card v-if="isLogin">
+        <v-card-title class="headline">発券完了</v-card-title>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import apiJobMixin from '@/mixins/apiJobMixin'
+export default {
+  mixins: [apiJobMixin]
+}
+</script>
